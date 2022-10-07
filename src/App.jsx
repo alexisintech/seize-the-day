b93a84897992d765a52f16e520911f850caecf5a
Alexis Aguilar <alexmerona@gmail.com>
2022-10-07 16:50:49 -0400

    rendering multiple pages using react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./style.css";
 import Index from "./pages/Index";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Index />,
+  },
+  {
+    path: "/signin",
+    element: <Signin />,
+  },
+  {
+    path: "/signup",
+    element: <Signup />,
   },
 ]);
 

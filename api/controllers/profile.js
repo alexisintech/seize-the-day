b1af90f575213f0aeae98394af795aacfb3541e1
Alexis Aguilar <alexmerona@gmail.com>
2022-10-13 16:58:57 -0400

    profile routes and controller with the Todo model

---
+++ api/controllers/profile.js
@@ -0,0 +1,61 @@
+const Todo = require("../models/Todo");
+
+const date = new Date();
+const dd = String(date.getDate()).padStart(2, '0');
+const mm = String(date.getMonth() + 1).padStart(2, '0'); // January is 0!
+const yyyy = date.getFullYear();
+const todaysDate = mm + '/' + dd + '/' + yyyy;
+// prefix integers below 10 with 0's 
+// example: 10:7 will become 10:07
+function addZero(i) {
+  if (i < 10) {i = "0" + i}
+  return i;
+}
+const currentTime = `${addZero(date.getHours())}:${addZero(date.getMinutes())}`
+
+module.exports = {
+  getTodos: async (req, res) => {
+    try {
+      const todos = await Todo.find()
+      
+      res.json(todos);
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  createTodo: async (req, res) => {
+    try {
+      const todo = await Todo.create({
+        text: req.body.text,
+      });
+      console.log("New todo was created!");
+      
+      todo.save();
+
+      res.json(todo);
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  deleteTodo: async (req, res) => {
+    try {
+      const result = await Todo.findByIdAndDelete(req.params.id);
+
+	    res.json({result});
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  completeTodo: async (req, res) => {
+    try {
+      const todo = await Todo.findById(req.params.id);
+      
+      todo.complete = !todo.complete;
+      todo.save();
+
+      res.json(todo);
+    } catch (err) {
+    console.log(err);
+    }
+  },
+}
---
+++ api/routes/profile.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+const profileController = require("../controllers/profile");
+
+// /todo Routes
+// treat each path as: /profile/createTodo  /profile/delete/:id  /profile/complete/:id
+
+router.get("/", profileController.getTodos)
+
+router.post("/createTodo", profileController.createTodo);
+
+router.put("/complete/:id", profileController.completeTodo);
+
+router.delete("/delete/:id", profileController.deleteTodo);
+
+module.exports = router;

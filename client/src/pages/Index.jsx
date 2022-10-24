1ee098dfbb0cc91c2d38f181a3d85c419033629a
Alexis Aguilar <alexmerona@gmail.com>
2022-10-24 16:25:09 -0400

    Update Index.jsx

--- client/src/pages/Index.jsx
+++ client/src/pages/Index.jsx
@@ -1,4 +1,6 @@
 import Button from "@mui/material/Button";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 const { palette } = createTheme();
@@ -6,29 +8,41 @@ const { augmentColor } = palette;
 const createColor = (mainColor) => augmentColor({ color: { main: mainColor } });
 const theme = createTheme({
   palette: {
-    primary: createColor("#FA347B"),
-    secondary: createColor("#8A4EFC"),
+    primary: createColor("#03015d"),
+    secondary: createColor("#1b1799"),
     light: createColor("#EEE"),
-    lightAlt: createColor("#61759b"),
-    dark: createColor("#131A26"),
-    darkAlt: createColor("#202B3E"),
   },
 });
 
 export default function Index() {
   return (
     <ThemeProvider theme={theme}>
-      <h1>Seize The Day</h1>
-      <form action="/login" method="get">
-        <Button variant="contained" type="submit" color="primary">
-          Login
-        </Button>
-      </form>
-      <form action="/signup" method="get">
-        <Button variant="outlined" type="submit" color="light">
-          Signup
-        </Button>
-      </form>
+      <Box className="hero">
+        <Box sx={{ paddingTop: 30, paddingLeft: 35 }}>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sx={{ marginBottom: 2, color: "#fff" }}>
+              <h1>Seize The Day</h1>
+            </Grid>
+            <Grid item xs={12} sx={{ marginBottom: 1, color: "#fff" }}>
+              Organize your day, mindfully.
+            </Grid>
+            <Grid item>
+              <form action="/login" method="get">
+                <Button variant="outlined" type="submit" color="light">
+                  Login
+                </Button>
+              </form>
+            </Grid>
+            <Grid item>
+              <form action="/signup" method="get">
+                <Button variant="contained" type="submit" color="secondary">
+                  Signup
+                </Button>
+              </form>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
     </ThemeProvider>
   );
 }

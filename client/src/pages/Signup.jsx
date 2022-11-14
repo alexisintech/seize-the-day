3e16394e7cec444e52bbd0cf2e4d7cb917a1c020
Alexis Aguilar <alexmerona@gmail.com>
2022-11-13 19:21:06 -0500

    Update Signup.jsx

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,10 +1,8 @@
-import * as React from "react";
+import { useState } from "react";
 import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
 import TextField from "@mui/material/TextField";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
 import Link from "@mui/material/Link";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
@@ -25,14 +23,39 @@ const theme = createTheme({
   },
 });
 
+const api_base =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:2222"
+    : "replace with railway URL";
+
 export default function Signup() {
-  const handleSubmit = (event) => {
+  const [doesUserExist, setDoesUserExist] = useState(false);
+
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get("email"),
-      password: data.get("password"),
-    });
+    const data = new FormData(event.target);
+
+    try {
+      const req = await fetch(api_base + "/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          userName: data.get("userName"),
+          email: data.get("email"),
+          password: data.get("password"),
+          confirmPassword: data.get("confirmPassword"),
+        }),
+      });
+      const res = await req.json();
+      console.log(res);
+      if (res.message === "User already exists!") {
+        setDoesUserExist(true);
+      }
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   return (
@@ -74,9 +97,9 @@ export default function Signup() {
                   autoFocus
                   required
                   fullWidth
-                  id="firstName"
-                  label="First Name"
-                  name="firstName"
+                  id="userName"
+                  label="Username"
+                  name="userName"
                   autoComplete="given-name"
                 />
               </Grid>
@@ -101,12 +124,29 @@ export default function Signup() {
                   autoComplete="new-password"
                 />
               </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="confirmPassword"
+                  label="Confirm Password"
+                  name="confirmPassword"
+                  type="password"
+                />
+              </Grid>
             </Grid>
+
+            {doesUserExist && (
+              <Typography sx={{ color: "red", mt: 2, fontSize: "0.9rem" }}>
+                Account with that email address or username already exists.
+              </Typography>
+            )}
+
             <Button
               type="submit"
               fullWidth
               variant="contained"
-              sx={{ mt: 3, mb: 2 }}
+              sx={{ mt: 2, mb: 2 }}
             >
               Sign Up
             </Button>

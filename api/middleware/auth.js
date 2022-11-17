64dfdfed863d123d92310c10f74023bf9a5a7756
Alexis Aguilar <alexmerona@gmail.com>
2022-11-16 19:59:30 -0500

    Update auth.js

--- api/middleware/auth.js
+++ api/middleware/auth.js
@@ -1,9 +1,24 @@
-module.exports = {
-  ensureAuth: function (req, res, next) {
-    if (req.isAuthenticated()) {
-      return next();
-    } else {
-      res.redirect("/");
-    }
-  },
+const jwt = require("jsonwebtoken");
+require("dotenv").config({ path: "./config/.env" });
+
+const authenticateToken = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (token === null)
+    return res.status(401).json({
+      message:
+        "You cannot complete this action because you aren't authenticated.",
+    });
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err)
+      return res.status(403).json({ message: "User was not authenticated." });
+
+    req.user = user;
+
+    next();
+  });
 };
+
+module.exports = authenticateToken;
